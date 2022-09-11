fccddf6507bcb813ab2f5aaa3d2a21ea7d00f139
AnWuPP <idonhomka@gmail.com>
2022-09-11 23:40:39 +0300

    remove test functional

--- Statements/Statements.cpp
+++ Statements/Statements.cpp
@@ -4,24 +4,13 @@ int WaitingPlayerActionStatements::get_id() const
 {
 	return 0;
 }
-void WaitingPlayerActionStatements::paint()
-{
-}
 
 int ShowPrizeStatements::get_id() const
 {
 	return 1;
 }
-void ShowPrizeStatements::paint()
-{
-	ImGui::Text("State 1");
-}
 
 int DrumRotationStatements::get_id() const
 {
 	return 2;
-}
-void DrumRotationStatements::paint()
-{
-	ImGui::Text("State 2");
 }
--- Statements/Statements.h
+++ Statements/Statements.h
@@ -6,36 +6,26 @@
 class Statements
 {
 public:
-	typename typedef Statements type_t;
-	using type = Statements;
 	virtual ~Statements() = default;
 
 	virtual int get_id() const = 0;
-	virtual void paint() = 0;
 };
 
 class WaitingPlayerActionStatements : public Statements
 {
 public:
-	typename typedef WaitingPlayerActionStatements type_t;
-	using type = WaitingPlayerActionStatements;
 	int get_id() const override;
-	void paint() override;
 };
 
 class ShowPrizeStatements : public Statements
 {
 public:
-	using type = ShowPrizeStatements;
 	int get_id() const override;
-	void paint() override;
 };
 
 class DrumRotationStatements : public Statements
 {
 public:
-	using type = ShowPrizeStatements;
 	int get_id() const override;
-	void paint() override;
 };
 
